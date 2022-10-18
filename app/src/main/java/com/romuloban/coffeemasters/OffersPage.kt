030e36c952a86db31683cb64ae57010f616c5612
Romulo Banares <romulo.banares.contractor@bbva.com>
2022-10-18 17:21:03 +0200

    modify offers page

--- app/src/main/java/com/romuloban/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/romuloban/coffeemasters/OffersPage.kt
@@ -3,6 +3,8 @@ package com.romuloban.coffeemasters
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,38 +19,50 @@ import androidx.compose.ui.unit.dp
 import com.romuloban.coffeemasters.ui.theme.Alternative1
 import com.romuloban.coffeemasters.ui.theme.Alternative2
 
-@Preview(showBackground = true, widthDp = 400)
+@Preview(showBackground = true)
 @Composable
-private fun Offer_Preview() {
-    Offer("My Title 1", "This the description")
+fun OffersPage() {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+    ) {
+        Offer(title = "Early Coffe", description = "10% off. Offer valid from 6am to 9am.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+    }
 }
 
 @Composable
 fun Offer(title: String, description: String) {
-    Image(painter = painterResource(R.drawable.background_pattern),
-        contentDescription = "Background pattern",
-        contentScale = ContentScale.FillWidth,
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(180.dp)
-    )
-    Column(
-        horizontalAlignment =  Alignment.CenterHorizontally,
-        modifier = Modifier.padding(16.dp)
+            .padding(16.dp)
     ) {
-        Text(text = title,
-            style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .background(Alternative2)
-                .padding(16.dp)
+        Image(painter = painterResource(R.drawable.background_pattern),
+            contentDescription = "Background pattern",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.matchParentSize()
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = description,
-            style = MaterialTheme.typography.h6,
+        Column(
+            horizontalAlignment =  Alignment.CenterHorizontally,
             modifier = Modifier
-                .background(Alternative2)
                 .padding(16.dp)
-        )
+                .fillMaxWidth()
+        ) {
+            Text(text = title,
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(Alternative2)
+                    .padding(16.dp)
+            )
+            Text(text = description,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .background(Alternative2)
+                    .padding(16.dp)
+            )
+        }
     }
 }
