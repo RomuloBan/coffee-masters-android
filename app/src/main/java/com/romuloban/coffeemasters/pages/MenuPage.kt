467be22b3f6596e8831b2725a0835b5af1525e49
romulo.banares <romulo88@hotmail.com>
2022-10-21 21:34:41 +0200

    add product item compose

--- app/src/main/java/com/romuloban/coffeemasters/pages/MenuPage.kt
+++ app/src/main/java/com/romuloban/coffeemasters/pages/MenuPage.kt
@@ -1,10 +1,76 @@
 package com.romuloban.coffeemasters.pages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.romuloban.coffeemasters.Product
+import com.romuloban.coffeemasters.R
+import com.romuloban.coffeemasters.ui.theme.Alternative1
 
 @Preview
 @Composable
 fun MenuPage() {
 
+}
+
+
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+@Preview
+@Composable
+fun ProductItem_Preview() {
+    ProductItem(product = Product(1, "Dummy", 1.25, ""), onAdd = {})
+}
+
+
+@Composable
+fun ProductItem(product: Product, onAdd: (Product)->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.black_coffee),
+            contentDescription = "Image for ${product.name}",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column {
+                Text(product.name, fontWeight = FontWeight.Bold)
+                Text("$${product.price.format(2)} ea")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Alternative1,
+                    contentColor = Color.White
+                ),
+                onClick = {
+                    onAdd(product)
+                },
+            ) {
+                Text("Add")
+            }
+        }
+    }
 }
