ad9eb116cecebc1bcace01eb2ced44dce6e9dbe7
romulo.banares <romulo88@hotmail.com>
2022-10-21 15:07:54 +0200

    add nav bar item

--- app/src/main/java/com/romuloban/coffeemasters/BottomNavigation.kt
+++ app/src/main/java/com/romuloban/coffeemasters/BottomNavigation.kt
@@ -1,11 +1,25 @@
 package com.romuloban.coffeemasters
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.romuloban.coffeemasters.ui.theme.Alternative1
+import com.romuloban.coffeemasters.ui.theme.OnPrimary
 
 data class NavPage(var name: String, var icon: ImageVector,
                    var route: String)
@@ -17,4 +31,31 @@ object Routes {
     val InfoPage = NavPage("Info", Icons.Outlined.Info, "info")
 
     val pages = listOf(MenuPage, OffersPage, OrderPage, InfoPage)
+}
+
+@Preview
+@Composable
+fun NavBarItem_Prev() {
+    NavBarItem(page = Routes.MenuPage, modifier = Modifier.padding(8.dp))
+}
+
+@Composable
+fun NavBarItem(page: NavPage, selected: Boolean = false, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(horizontal = 12.dp)) {
+        Image(
+            imageVector = page.icon,
+            contentDescription = page.name,
+            colorFilter = ColorFilter.tint(
+                if (selected) Alternative1 else OnPrimary
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .size(24.dp)
+        )
+        Text(page.name,
+            fontSize = 12.sp,
+            color = if (selected) Alternative1 else OnPrimary
+        )
+    }
 }
